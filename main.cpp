1f17438ff6e4006a8951813a8d7b3743fcfae53d
urrfd <145345379+urrfd@users.noreply.github.com>
2025-09-03 23:55:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,8 @@ int main(void) {
     InitWindow(800, 600, "Quiz + Minigames");
     SetTargetFPS(60);
 
-
-
-
-    // things to collect
-
-        Rectangle obs[3] = {
+    // Things to collect
+    Rectangle trash[3] = {
         { -50, 150, 40, 40 },
         { -100, 300, 50, 50 },
         { -200, 450, 30, 30 }
@@ -22,21 +18,16 @@ int main(void) {
         { 4, 0 }
     };
 
-
-
-    
-    GameScreen currentScreen = MENU;
-    bool shouldExit = false;
-    
-    
-
+    // Track collected state
+    bool collected[3] = { false, false, false };
 
     // Player for the dodge game
     Vector2 player = {400, 300};
-    Vector2 mouse = GetMousePosition();
-    Rectangle mousehitbox = {mouse.x, mouse.y, 10, 10};
 
-    // Victory message (changes depending on minigame)
+    GameScreen currentScreen = MENU;
+    bool shouldExit = false;
+
+    // Victory message
     const char *victoryMsg = "YOU WIN!";
 
     while (!WindowShouldClose() && !shouldExit) {
@@ -51,12 +42,11 @@ int main(void) {
                     player = (Vector2){400, 300}; // reset player
                 }
                 if (IsKeyPressed(KEY_THREE)) {
-                    shouldExit = true;
+                    currentScreen = COLLECT;
                 }
                 break;
 
             case QUIZ:
-                // Placeholder: win instantly when you press SPACE
                 if (IsKeyPressed(KEY_SPACE)) {
                     currentScreen = VICTORY;
                     victoryMsg = "You beat the QUIZ!";
@@ -67,13 +57,11 @@ int main(void) {
                 break;
 
             case DODGE:
-                // Move square
                 if (IsKeyDown(KEY_RIGHT)) player.x += 4;
                 if (IsKeyDown(KEY_LEFT))  player.x -= 4;
                 if (IsKeyDown(KEY_UP))    player.y -= 4;
                 if (IsKeyDown(KEY_DOWN))  player.y += 4;
 
-                // Victory condition: reach right edge
                 if (player.x > 760) {
                     currentScreen = VICTORY;
                     victoryMsg = "You won the DODGE game!";
@@ -83,6 +71,39 @@ int main(void) {
                     currentScreen = MENU;
                 }
                 break;
+
+            case COLLECT: {
+                // Update mouse hitbox
+                Vector2 mouse = GetMousePosition();
+                Rectangle mousehitbox = {mouse.x, mouse.y, 10, 10};
+
+                for (int i = 0; i < 3; i++) {
+                    // Move trash if not collected
+                    if (!collected[i]) {
+                        trash[i].x += velocity[i].x;
+
+                        // Wrap around when off screen
+                        if (trash[i].x > GetScreenWidth()) {
+                            trash[i].x = -trash[i].width;
+                        }
+
+                        // Check collection
+                        if (CheckCollisionRecs(mousehitbox, trash[i]) &&
+                            IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                            collected[i] = true;
+
+                            // Respawn at left side, random y position
+                            trash[i].x = -trash[i].width;
+                            trash[i].y = GetRandomValue(100, 500);
+                            collected[i] = false;
+                        }
+                    }
+                }
+
+                if (IsKeyPressed(KEY_ESCAPE)) {
+                    currentScreen = MENU;
+                }
+            } break;
 
             case VICTORY:
                 if (IsKeyPressed(KEY_M)) {
@@ -98,9 +119,9 @@ int main(void) {
         switch (currentScreen) {
             case MENU:
                 DrawText("== MAIN MENU ==", 260, 100, 40, DARKBLUE);
-                DrawText("Press [1] Quiz",       260, 200, 30, DARKGRAY);
-                DrawText("Press [2] Dodge Game", 260, 250, 30, DARKGRAY);
-                DrawText("Press [3] Exit",       260, 300, 30, DARKGRAY);
+                DrawText("Press [1] Quiz",        260, 200, 30, DARKGRAY);
+                DrawText("Press [2] Dodge Game",  260, 250, 30, DARKGRAY);
+                DrawText("Press [3] Collect Game",260, 300, 30, DARKGRAY);
                 break;
 
             case QUIZ:
@@ -117,11 +138,18 @@ int main(void) {
                 DrawRectangleV(player, (Vector2){40, 40}, BLUE);
                 break;
 
-            case COLLECT:
-                DrawText("move your mouse and collect all items to win!", 220, 100, 20, DARKGRAY);
-                DrawText("Press [ESC] to return", 250, 140, 20, DARKGRAY);
-                
-                break;
+            case COLLECT: {
+                DrawText("Click the flying red squares to collect them!", 120, 40, 20, DARKGRAY);
+
+                Vector2 mouse = GetMousePosition();
+                Rectangle mousehitbox = {mouse.x, mouse.y, 10, 10};
+
+                for (int i = 0; i < 3; i++) {
+                    DrawRectangleRec(trash[i], RED);
+                }
+
+                DrawRectangleRec(mousehitbox, BLUE); // Mouse hitbox
+            } break;
 
             case VICTORY:
                 DrawText(victoryMsg, 200, 200, 40, GREEN);
