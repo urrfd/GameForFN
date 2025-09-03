1c0417b5fc86cde570310dcb5520e612f735298e
urrfd <145345379+urrfd@users.noreply.github.com>
2025-09-03 16:32:30 +0200

    Update main.cpp
    a simple build wow switches are cool

--- main.cpp
+++ main.cpp
@@ -1,37 +1,102 @@
 #include "raylib.h"
 
-typedef enum GameScreen { MENU, QUIZ } GameScreen;
+typedef enum GameScreen { MENU, QUIZ, DODGE, VICTORY } GameScreen;
 
 int main(void) {
-    // Window setup
-    InitWindow(800, 600, "Quiz Game");
+    InitWindow(800, 600, "Quiz + Minigames");
     SetTargetFPS(60);
 
     GameScreen currentScreen = MENU;
+    bool shouldExit = false;
 
-    // Main loop
-    while (!WindowShouldClose()) {
-        // Update
-        if (currentScreen == MENU) {
-            if (IsKeyPressed(KEY_ONE)) currentScreen = QUIZ;  // Press 1 to start
-            if (IsKeyPressed(KEY_TWO)) break;                 // Press 2 to exit
-        }
-        else if (currentScreen == QUIZ) {
-            if (IsKeyPressed(KEY_ESCAPE)) currentScreen = MENU; // Go back to menu
+    // Player for the dodge game
+    Vector2 player = {400, 300};
+
+    // Victory message (changes depending on minigame)
+    const char *victoryMsg = "YOU WIN!";
+
+    while (!WindowShouldClose() && !shouldExit) {
+        // --- UPDATE ---
+        switch (currentScreen) {
+            case MENU:
+                if (IsKeyPressed(KEY_ONE)) {
+                    currentScreen = QUIZ;
+                }
+                if (IsKeyPressed(KEY_TWO)) {
+                    currentScreen = DODGE;
+                    player = (Vector2){400, 300}; // reset player
+                }
+                if (IsKeyPressed(KEY_THREE)) {
+                    shouldExit = true;
+                }
+                break;
+
+            case QUIZ:
+                // Placeholder: win instantly when you press SPACE
+                if (IsKeyPressed(KEY_SPACE)) {
+                    currentScreen = VICTORY;
+                    victoryMsg = "You beat the QUIZ!";
+                }
+                if (IsKeyPressed(KEY_ESCAPE)) {
+                    currentScreen = MENU;
+                }
+                break;
+
+            case DODGE:
+                // Move square
+                if (IsKeyDown(KEY_RIGHT)) player.x += 4;
+                if (IsKeyDown(KEY_LEFT))  player.x -= 4;
+                if (IsKeyDown(KEY_UP))    player.y -= 4;
+                if (IsKeyDown(KEY_DOWN))  player.y += 4;
+
+                // Victory condition: reach right edge
+                if (player.x > 760) {
+                    currentScreen = VICTORY;
+                    victoryMsg = "You won the DODGE game!";
+                }
+
+                if (IsKeyPressed(KEY_ESCAPE)) {
+                    currentScreen = MENU;
+                }
+                break;
+
+            case VICTORY:
+                if (IsKeyPressed(KEY_M)) {
+                    currentScreen = MENU;
+                }
+                break;
         }
 
-        // Draw
+        // --- DRAW ---
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        if (currentScreen == MENU) {
-            DrawText("== QUIZ GAME ==", 260, 100, 40, DARKBLUE);
-            DrawText("Press [1] Start Quiz", 260, 200, 30, DARKGRAY);
-            DrawText("Press [2] Exit",       260, 250, 30, DARKGRAY);
-        }
-        else if (currentScreen == QUIZ) {
-            DrawText("QUIZ SCREEN!", 280, 200, 40, RED);
-            DrawText("Press [ESC] to go back", 250, 300, 30, DARKGRAY);
+        switch (currentScreen) {
+            case MENU:
+                DrawText("== MAIN MENU ==", 260, 100, 40, DARKBLUE);
+                DrawText("Press [1] Quiz",       260, 200, 30, DARKGRAY);
+                DrawText("Press [2] Dodge Game", 260, 250, 30, DARKGRAY);
+                DrawText("Press [3] Exit",       260, 300, 30, DARKGRAY);
+                break;
+
+            case QUIZ:
+                DrawText("QUIZ SCREEN!", 280, 200, 40, RED);
+                DrawText("Press [SPACE] to WIN", 250, 300, 30, DARKGRAY);
+                DrawText("Press [ESC] to return", 250, 350, 30, DARKGRAY);
+                break;
+
+            case DODGE:
+                DrawText("DODGE GAME!", 280, 50, 40, RED);
+                DrawText("Reach the right side to win!", 200, 100, 20, DARKGRAY);
+                DrawText("Press [ESC] to return", 250, 140, 20, DARKGRAY);
+
+                DrawRectangleV(player, (Vector2){40, 40}, BLUE);
+                break;
+
+            case VICTORY:
+                DrawText(victoryMsg, 200, 200, 40, GREEN);
+                DrawText("Press [M] to return to Menu", 200, 300, 30, DARKGRAY);
+                break;
         }
 
         EndDrawing();
